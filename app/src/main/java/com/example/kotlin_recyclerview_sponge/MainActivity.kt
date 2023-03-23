85c93466536d4015c7ee836b9db7d2c22e3ced84
Iyad Abu Doush <dr.iyad.abudoush@gmail.com>
2023-03-23 08:21:29 -0800

    The Kotlin RecyclerView example using SpongeBob characters

--- app/src/main/java/com/example/kotlin_recyclerview_sponge/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_recyclerview_sponge/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,20 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // getting the recyclerview by its id
-        val recyclerView = findViewById<RecyclerView>(R.id.listCharacters)
+        // getting the array of Strings from XML
         val charStringArray = getResources().getStringArray(R.array.characters)
 
+        //ArrayList of Item objects filled with names and images
         val items: ArrayList<Item> = ArrayList<Item>()
-        //val items =  List<Item>
         items.add(Item(charStringArray[0], R.drawable.spongebob))
         items.add(Item(charStringArray[1], R.drawable.patrick))
         items.add(Item(charStringArray[2], R.drawable.sandy))
         items.add(Item(charStringArray[3], R.drawable.squedword))
 
-        recyclerView.setLayoutManager(LinearLayoutManager(this))
+        //set the layout of the contents, i.e. list of repeating views in the recycler view.
+        //without it, RecyclerView will not function i.e. there is no out of the box default.
+        listCharacters.setLayoutManager(LinearLayoutManager(this))
 
+        //Pass the items object to the defined adapter class
         val adapter = MyAdapter(items)
+
+        //define the method on the onItemClickListner of the adapter
         adapter.setOnItemClickListener(object : MyAdapter.onItemClickListener{
             override fun onItemClick(position: Int) {
                 Toast.makeText(this@MainActivity, "The Selected Item is "+position,Toast.LENGTH_SHORT).show()
@@ -35,7 +39,8 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        recyclerView.adapter = adapter
+        //Assign the defined adapter object to the RecyclerView adapter
+        listCharacters.adapter = adapter
 
     }
 }
--- app/src/main/java/com/example/kotlin_recyclerview_sponge/MyAdapter.kt
+++ app/src/main/java/com/example/kotlin_recyclerview_sponge/MyAdapter.kt
@@ -7,6 +7,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+//We must implement three methods of the RecyclerView.Adapter
+//1- onCreateViewHolder
+//2- onBindViewHolder
+//3- getItemCount
 class MyAdapter(private val items: List<Item>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     private lateinit var mListener: onItemClickListener
@@ -22,20 +26,26 @@ class MyAdapter(private val items: List<Item>) : RecyclerView.Adapter<MyAdapter.
     }
 
     // return a viewholder, and inflate the layout we created
+    //only creates a new view holder when there are no existing view holders which the RecyclerView can reuse.
+    // So, for instance, if your RecyclerView can display 5 items at a time, it will create 5-6 ViewHolders,
+    // and then automatically reuse them, each time calling onBindViewHolder.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_view,parent,false), mListener);
     }
     //set items to the view
+    //Note that instead of creating new view for each new row, an old view is recycled and reused by binding new data to it.
+    //Initially you will get new unused view holders and you have to fill them with data you want to display.
+    // But as you scroll you'll start getting view holders that were used for rows that went off screen
+    // and you have to replace old data that they held with new data.
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val item = items[position]
-        holder.nameView.setText(items?.get(position)?.getName())
-        holder.imageView.setImageResource(items?.get(position)!!.getImage())
-
+        holder.nameView.setText(items.get(position).getName())
+        holder.imageView.setImageResource(items.get(position).getImage())
     }
 
-    // return count of total items we have created
+    //It returns The number of items currently available in adapter.
     override fun getItemCount(): Int {
-        return items!!.size
+        return items.size
     }
 
     class MyViewHolder(itemView: View, clickListener: onItemClickListener) : RecyclerView.ViewHolder(itemView) {
@@ -51,6 +61,5 @@ class MyAdapter(private val items: List<Item>) : RecyclerView.Adapter<MyAdapter.
                 clickListener.onItemClick(adapterPosition)
             }
         }
-
     }
 }
